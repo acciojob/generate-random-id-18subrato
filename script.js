a0c0eec5a6a3befd529081cd898f8476d3e0c49f
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-09 13:44:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
-  // write your code here
+	l = Number(l);
+	let str = 'abcdefghizklmnopqrstwxyzABCDEFGHIJKLMONPQRSTWXYZ123456789';
+	let newStr = '';
+
+	for(let i=0;i<l;i++){
+		newStr += str[Math.floor(Math.random()*str.length)]
+	}
+	return newStr;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+const l = prompt("Enter a number.")
 alert(makeid(l));
